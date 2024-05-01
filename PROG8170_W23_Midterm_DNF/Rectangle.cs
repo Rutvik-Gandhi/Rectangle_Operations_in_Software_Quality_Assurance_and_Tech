dc06c5ffbcf91781e65d8342e743961dd9ce4888
Rutvik-Gandhi <113471827+Rutvik-Gandhi@users.noreply.github.com>
2024-05-01 17:37:19 -0400

    Add files via upload

---
+++ PROG8170_W23_Midterm_DNF/Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8170_W23_Midterm_DNF
+{
+    public class Rectangle
+    {
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+        public Rectangle(int l, int b)
+        {
+            length = l;
+            width = b;
+        }
+        private int length;
+        public int Length
+        {
+            get { return length; }
+        }
+        private int width;
+        public int Width
+        {
+            get { return width; }
+        }
+        public int GetLength()
+        {
+            return Length;
+        }
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return Length;
+        }
+        public int GetWidth()
+        {
+            return Width;
+        }
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return Width;
+        }
+        public int GetPerimeter()
+        {
+            return (2 * (Length + Width));
+        }
+        public int GetArea()
+        {
+            return (Length * Width);
+        }
+    }
+}
